0c7510f0b1dade2b90198c05e1bdb4f0beae87d5
anshul152003 <anshulgangrade2k3@gmail.com>
2025-05-28 12:42:51 +0530

    c

--- Frontend/js/class-11/script.js
+++ Frontend/js/class-11/script.js
@@ -1,4 +1,4 @@
-/* const cooler={
+const cooler={
     name: "Cooler",
     id:"D223",
     functionality:{
@@ -8,28 +8,14 @@
     height:3.5
 
 };
-// const dublicate=cooler;
+// const dublicate=cooler; 
 const dublicate=Object.assign({},cooler); // shallow copy
+// const dublicate=JSON.parse(JSON.stringify(cooler)); // deep copy
 dublicate.name="dublicate cooler";
 console.log("original: ",cooler);
 console.log("dublicate" ,dublicate);
+
 dublicate.functionality.color="black";
 cooler.brand="voltas";
 console.log("after update original: ",cooler);
-console.log("after update dublicate: ",dublicate); */
-
-const cooler1={
-    name: "bluecooler",
-    id:"D223QSW",
-    functionality:{
-        kaam: "cooling",
-        color:"white  "  
-    },
-    height:3.5
-};
-
-const dublicate1=JSON.parse(JSON.stringify(cooler1)); // deep copy
-dublicate1.id = "1011";
-
-console.log("original: ",cooler1);
-console.log("dublicate1" ,dublicate1);
+console.log("after update dublicate: ",dublicate); 
