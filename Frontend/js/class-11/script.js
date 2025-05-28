25e23687cd0e3b953d2a775a49c84fbcf9bf6734
anshul152003 <anshulgangrade2k3@gmail.com>
2025-05-28 16:32:46 +0530

    C

--- Frontend/js/class-11/script.js
+++ Frontend/js/class-11/script.js
@@ -1,4 +1,4 @@
-const cooler={
+/*const cooler={
     name: "Cooler",
     id:"D223",
     functionality:{
@@ -19,3 +19,26 @@ dublicate.functionality.color="black";
 cooler.brand="voltas";
 console.log("after update original: ",cooler);
 console.log("after update dublicate: ",dublicate); 
+
+*/
+
+//shallow copy pass by address
+const cooler1={
+    name: "blu",
+    id: "DE223",
+    functionality: {
+        kaam: "cooling",
+        color: "white"
+    },
+    height: 3.5
+};
+
+// const cooler2 = cooler1; // shallow copy
+// const cooler2 = Object.assign({}, cooler1); 
+const cooler2 = {...cooler1 }; // spread operator shallow copy
+cooler2.id = "5555"; 
+cooler2.name = "dublicate cooler";
+cooler2.functionality.color = "green";
+
+console.log("cooler1: ", cooler1);
+console.log("cooler2: ", cooler2);
