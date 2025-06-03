81796712baf80f4b4dfe6dbaa88d20f508195f47
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-03 11:20:10 +0530

    c

--- Frontend/js/class-15/script.js
+++ Frontend/js/class-15/script.js
@@ -3,4 +3,19 @@
 //closure : 
 
 //XHR :
+const ans = new XHR();
+ans.responseType = "json";
+ans.open("GET", "https://jsonplaceholder.typicode.com/posts");
+ans.send();
+console.log(ans);
 
+//Dynamic Function Creation:
+function CA(me, URL) {
+    const ans = new XHR();
+    ans.responseType = "json";
+    ans.open(me,URL);
+    ans.send();
+    console.log(ans);
+}
+
+//callback Hell :
