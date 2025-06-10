2a4a0366e56b6bf96aa99cbd3d00115eea43ebea
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-10 11:37:44 +0530

    c

--- Frontend/js/DOM/class-3/script.js
+++ Frontend/js/DOM/class-3/script.js
@@ -1,2 +1,22 @@
 const myDiv = document.getElementById("onlyDiv");
 console.log(myDiv);
+
+console.log(myDiv.attributes);
+
+for(let i = 0; i < myDiv.attributes.length; i++) {
+    console.log(myDiv.attributes[i].name + " = " + myDiv.attributes[i].value);
+}
+
+//if you want to change an attribute
+myDiv.attributes[0].value = "New Value";
+for(let i = 0; i < myDiv.attributes.length; i++) {
+    console.log(myDiv.attributes[i]);
+}
+
+//if you want to add new attribute
+myDiv.setAttribute("data-custom", "Custom Value");
+console.log(myDiv.getAttribute("data-custom"));
+
+//if you want to remove an attribute
+myDiv.removeAttribute("data-custom");
+console.log(myDiv.getAttribute("data-custom")); // should be null now
