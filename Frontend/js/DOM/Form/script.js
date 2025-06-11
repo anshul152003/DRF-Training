b404ac3acad25ba9d61f1dc9d15725cb54b2422d
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-11 11:16:29 +0530

    c

--- Frontend/js/DOM/Form/script.js
+++ Frontend/js/DOM/Form/script.js
@@ -1,41 +1,31 @@
 const form = document.getElementById("form");
-const element = form.elements;
-const display = document.getElementById("display");
+const card = document.getElementById("card");
+const formSection = document.getElementById("container");
+
+const fnameInput = document.getElementById("fname");
+const lnameInput = document.getElementById("lname");
+const emailInput = document.getElementById("email");
+const courseInput = document.getElementById("course");
+
+const cardFirst = document.getElementById("cardFirstName");
+const cardLast = document.getElementById("cardLastName");
+const cardEmail = document.getElementById("cardEmail");
+const cardCourse = document.getElementById("cardCourse");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    displayData();
-    form.reset();
+
+    cardFirst.innerText = fnameInput.value;
+    cardLast.innerText = lnameInput.value;
+    cardEmail.innerText = emailInput.value;
+    cardCourse.innerText = courseInput.value;
+
+    form.style.display = "none";
+    card.style.display = "block";
 });
 
-function displayData() {
-    const div = document.createElement("div");
-    const div1 = document.createElement("div");
-
-    const fname = document.createElement("div");
-    fname.innerHTML = "FIRST-NAME: " + element[0].value;
-
-    const lname = document.createElement("div");
-    lname.innerHTML = "LAST-NAME: " + element[1].value;
-
-    const gender = document.createElement("div");
-    if (element[2].checked) gender.innerHTML = "Gender: Male";
-    else if (element[3].checked) gender.innerHTML = "Gender: Female";
-
-    const email = document.createElement("div");
-    email.innerHTML = "EMAIL: " + element[4].value;
-
-    const skills = document.createElement("div");
-    let skillArr = [];
-    if (element[5].checked) skillArr.push("HTML");
-    if (element[6].checked) skillArr.push("CSS");
-    if (element[7].checked) skillArr.push("JS");
-    skills.innerHTML = "SKILLS: " + skillArr.join(", ");
-
-    div1.append(fname, lname, gender, email, skills);
-    div.appendChild(div1);
-    display.appendChild(div);
-
-    div1.classList.add("div1");
-    display.classList.add("display");
+function showform() {
+    form.reset();
+    formSection.style.display = "block";
+    card.style.display = "none";
 }
