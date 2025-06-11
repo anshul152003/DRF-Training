153c7f901e76184af879a9e60bda69b8396622da
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-11 12:41:27 +0530

    c

--- Frontend/js/DOM/Clock/script.js
+++ Frontend/js/DOM/Clock/script.js
@@ -4,6 +4,7 @@ function rotateClockHands() {
   const secondHand = document.getElementById('second');
 
   const now = new Date();
+  console.log(now);
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
@@ -12,9 +13,9 @@ function rotateClockHands() {
   const minuteDeg = minutes * 6 + seconds * 0.1;
   const hourDeg = hours * 30 + minutes * 0.5; 
 
-  secondHand.style.transform = `rotate(${secondDeg}deg)`;
-  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+//   secondHand.style.transform = `rotate(${secondDeg}deg)`;
+//   minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+//   hourHand.style.transform = `rotate(${hourDeg}deg)`;
 }
 
 setInterval(rotateClockHands, 1000);
