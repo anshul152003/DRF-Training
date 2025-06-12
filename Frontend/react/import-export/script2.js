e7d1e1e0c6fd6c07b1f5ff6574c3e65f352f2684
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-12 15:41:46 +0530

    commit

--- Frontend/react/import-export/script2.js
+++ Frontend/react/import-export/script2.js
@@ -1,6 +1,7 @@
 import {greet} from "./script1.js"; //when export is written inline
 import {name} from "./script1.js"; //when export is written inline
 import hello from "./script1.js"; //when export is written separately
+
 hello(); 
 greet();
 console.log(`Name imported from script1.js: ${name}`);
