c108f5855cdbbaeeddc2e675592a764c6a85d47b
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-12 15:40:54 +0530

    commit

--- Frontend/react/import-export/script1.js
+++ Frontend/react/import-export/script1.js
@@ -1,3 +1,9 @@
+export const name = "Anshul Gangrade";
+
+export function greet() {
+    console.log(`Hello, ${name}!`);
+}
+
 function hello(){
     console.log("Hello, World!");
 }
--- Frontend/react/import-export/script2.js
+++ Frontend/react/import-export/script2.js
@@ -1,2 +1,6 @@
-import {hello} from "./script1.js";
+import {greet} from "./script1.js"; //when export is written inline
+import {name} from "./script1.js"; //when export is written inline
+import hello from "./script1.js"; //when export is written separately
 hello(); 
+greet();
+console.log(`Name imported from script1.js: ${name}`);
