c0e9b9c9036177f5e73ff139bc4693991652c209
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-13 11:32:51 +0530

    commit

--- Frontend/react/class1c/src/App.js
+++ Frontend/react/class1c/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
 function App() {
+  let name = "Anshul Gangrade";
+
+  function change(e) {
+    let name = "Anshul Kumar";
+    console.log(name);
+  }
+  
+  console.log(name);
+
   return (
-    <div className="App">
-      <h1>Anshul Here</h1>
+    <div>
+
+      <p>Anshul Here</p>
+      <p>My name is {name}.</p>
+      {/* <button onClick={() => {change()}}>Anonymous Name</button>  This will pass an anonymous function that calls changeName */}
+      {/* <button onClick={change()}>parenthese Name</button> This will call the function immediately */}
+      <button onClick={change}>Change Name</button>       This will pass the function reference
+
     </div>
   );
 }
