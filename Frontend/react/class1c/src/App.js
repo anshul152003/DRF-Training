087e1d67ab151e340a57974407bb0a16a171f015
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-13 11:50:59 +0530

    commit

--- Frontend/react/class1c/src/App.js
+++ Frontend/react/class1c/src/App.js
@@ -1,4 +1,31 @@
+// Types of Components in React:
+// 1. Stateless Components (Functional Components without state):
+// 2. Stateful Components (Using useState or Class Components):
+
+// Stateful Component Example using useState
+import React, { useState } from 'react';
 function App() {
+
+  const [stateName, setStateName] = useState("Anshul");
+
+  // function change(){
+  //   let newName = "Anshul Gangrade";
+  //   setStateName(newName);
+  // }
+
+  return (
+    <div>
+      <p>Anshul Here</p>
+      <p>My Full name is {stateName}</p>
+      <button onClick={() => {setStateName("Anshul Gangrade")}}>Change Name</button>
+    </div>
+  )
+}
+
+export default App;
+
+// Example of a Stateless Functional Component
+/*function App() {
   let name = "Anshul Gangrade";
 
   function change(e) {
@@ -10,15 +37,15 @@ function App() {
 
   return (
     <div>
-
       <p>Anshul Here</p>
       <p>My name is {name}.</p>
-      {/* <button onClick={() => {change()}}>Anonymous Name</button>  This will pass an anonymous function that calls changeName */}
-      {/* <button onClick={change()}>parenthese Name</button> This will call the function immediately */}
-      <button onClick={change}>Change Name</button>       This will pass the function reference
+      /* <button onClick={() => {change()}}> Anonymous Name </button>  This will pass an anonymous function that calls changeName */
+      /* <button onClick={change()}> parenthese Name </button> This will call the function immediately */
+      /* <button onClick={change}> Change Name </button>       This will pass the function reference*/
 
-    </div>
+/*    </div>
   );
 }
 
 export default App;
+*/
