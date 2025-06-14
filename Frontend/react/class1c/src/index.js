961fa65f4a75b1c27ebde46b8f84eb91255d1e84
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-14 09:58:59 +0530

    commit

--- Frontend/react/class1c/src/index.js
+++ Frontend/react/class1c/src/index.js
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import Form from './Form';
-import Change from './Change';
+// import Change from './Change';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <App />
   // <Form />
-  <Change/>
+  // <Change/>
+  <App />
 );
+
