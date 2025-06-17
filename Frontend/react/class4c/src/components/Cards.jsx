f7aa81004de6f7a22543be196631da790b8ffbf3
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-17 13:44:45 +0530

    commit

--- Frontend/react/class4c/src/components/Cards.jsx
+++ Frontend/react/class4c/src/components/Cards.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './Card.css'
 
 function Card({data}){
+
+    const [trackBtn, setTrackBtn] = useState(true);
     return (
         <div className="App">
             <img src={data?.image} alt="Food" />
@@ -10,7 +13,25 @@ function Card({data}){
                 <div>Made By: {data?.madeBy}</div>
                 <div>Price: ${data?.productPrice} <span>${data?.actualPrice}</span></div>
                 <div>Quantity: {data?.quantity}</div>
-                <div>Description: {data?.description}</div>
+                {/* <div>Description: {data?.description}</div> */}
+                <div>
+                    {trackBtn === true ? (
+                        <div>
+                            <p>
+                                {data?.description.substr(0,75)}
+                                <div>
+                                    <button onClick={()=>{setTrackBtn(!trackBtn)}}>Read More</button>
+                                </div>
+                            </p>
+                        </div> ) : (
+                        <div>
+                            <p>
+                                {data?.description}
+                            </p>
+                                <button onClick={() => {setTrackBtn(!trackBtn)}}>Read Less</button>
+                        </div>)}
+                </div>
+                        
             </div>
         </div>
     )
