51b9eeab64e791de87f1f99c3ce590596b63e7d6
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-24 15:07:42 +0530

    commit

--- Frontend/react/class9c2/src/components/Navbar.jsx
+++ Frontend/react/class9c2/src/components/Navbar.jsx
@@ -1,41 +1,37 @@
-
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import { useContext } from 'react';
 import { bitesContext } from '../context/MyContext';
 
 function Navbar() {
-    const {cartItem}=useContext(bitesContext)
-    console.log("Navbar",cartItem)
-    return (
+  const { cartItem } = useContext(bitesContext);
+  console.log("Navbar", cartItem);
 
-        <nav className="navbar">
-            <ul className="nav-list-containet">
-                <div className="nav-list">
-                    <li>
-                        <NavLink to="/" className="active-link">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about" className="active-link">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact" className="active-link">Contact</NavLink>
-                    </li>
-                </div>
-                <div className='Mycart'>
-                    <li>
-                        <NavLink to="/mycart" className="active-link">MyCart
-                            {
-                                 cartItem?.length > 0 ? (<p>{cartItem?.length}</p>) : ("") 
-                            }
-
-
-                        </NavLink>
-                    </li>
-                </div>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <ul className="nav-list-container">
+        <div className="nav-list">
+          <li>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : ""}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={({ isActive }) => isActive ? "active-link" : ""}>Contact</NavLink>
+          </li>
+        </div>
+        <div className="my-cart">
+          <li>
+            <NavLink to="/mycart" className={({ isActive }) => isActive ? "active-link" : ""}>
+              🛒 MyCart
+              {cartItem?.length > 0 && <span className="cart-badge">{cartItem.length}</span>}
+            </NavLink>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
