b2aaa97219dae146a4daf79017a5953d80a516e8
anshul152003 <anshulgangrade2k3@gmail.com>
2025-07-02 12:06:16 +0530

    commit

--- Backend/class2c1/controller/CreateCourse.js
+++ Backend/class2c1/controller/CreateCourse.js
@@ -3,7 +3,10 @@ const Course = require("../modals/Course")
 async function CreateCourse(req, res) {
     try {
         const { CourseName, CourseDuration, CourseAuthor, CourseRating } = req.body();
-        const res = await Course.create({ CourseName, CourseDuration, CourseAuthor, CourseRating })
+        const res1 = await Course.create({ CourseName, CourseDuration, CourseAuthor, CourseRating });
+        res1.status(201).json({
+            message: "Course Created", data: res1
+        });
     } catch(error) {
         console.error("Course not Create", error);
     }
