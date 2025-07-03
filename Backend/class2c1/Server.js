902cc763021fe167c7caafa7770406d515d334e4
anshul152003 <anshulgangrade2k3@gmail.com>
2025-07-03 09:51:07 +0530

    commit

--- Backend/class2c1/Server.js
+++ Backend/class2c1/Server.js
@@ -3,16 +3,15 @@ const express = require("express");
 require("dotenv").config();
 const DataBaseConnection = require("./config/DataBaseConnect.js");
 const CourseRoute = require("./routes/CourseRoute.js")
+
 // step-2 instance creation
 const app = express();
-// step-3server ko start karunga
 
+// step-3server ko start karunga
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
-
-
 
 //for getting the data from the req.body u have to use middle ware 
 app.use(express.json())
@@ -22,10 +21,8 @@ app.get("/", (req, res) => {
     res.send("<h1>Our Project now live</h1>")
 })
 
-
 //step-5 Data base connection 
 DataBaseConnection();
-
 
 //step-6 call here routes 
 app.use("/course/api/v1", CourseRoute)
