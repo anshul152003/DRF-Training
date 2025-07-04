5f3a8855e1337c2e19a0b26657f0647b72815036
anshul152003 <anshulgangrade2k3@gmail.com>
2025-07-04 15:49:45 +0530

    commit

--- Backend/class4c1/controllers/CreateUser.js
+++ Backend/class4c1/controllers/CreateUser.js
@@ -2,7 +2,19 @@ const User = require("../models/User");
 
 async function CreateUser(req, res){
     try{
-                
+        const { fName, lName, age, email, password } = req.body;
+        console.log("Here Data", fName, lName, age, email, password);
+
+        if( fName === "" && lName === "" && email === ""){
+            return res.status(500).json({
+                message : "First - Last Name and email should not be empty"
+            })
+        }
+        const UserCreated = await User.create({ fName, lName, age, email, password });
+        res.status(201).json({
+            message : "Success",
+            data : UserCreated
+        })
     }
     catch(error){
         console.error("User Signup failed", error);
--- Backend/class4c1/models/User.js
+++ Backend/class4c1/models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    userName: {type: String, maxLength:50, require: true},
-    userAge: {type: Number, maxLength:50, require: true},
-    userEmail: {type: String, maxLength:50, require: true},
-    userPassword: {type: String, maxLength:20, require: true}
+    fName: {type: String, maxLength:20, require: true},
+    lName: {type: String, maxLength:20, require: true},
+    age: {type: Number, require: true},
+    email: {type: String, maxLength:50, require: true},
+    password: {type: String, maxLength:20, require: true}
 });
 
 module.exports = mongoose.model("User", UserSchema);
